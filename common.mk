f0844665068761981bfa1c007dd312d4ca59ade2
bheatleyyy <sugakesshaa@pixelos.net>
2024-03-11 17:27:24 +0100

    sm8450-common: Import powerhint.json from lahaina
    from https://github.com/PixelOS-Devices/device_oneplus_sm8350-common/blob/fourteen-qpr1/configs/powerhint_lahaina.json
    
    Change-Id: I2ddb226cfd379b6b287a4393058c12dd579d38de

--- common.mk
+++ common.mk
@@ -366,6 +366,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr \
     android.hardware.power.stats@1.0
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # QCC
 PRODUCT_PACKAGES += \
     libgrpc++_unsecure.vendor
