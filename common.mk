ea6756f369a9e412a789cf8ba36e98c9ba9c601d
Rahul <rahul@aospa.co>
2024-03-23 15:12:16 +0000

    sm8450-common: Revert vulkan changes
    * This causes green artifacts to appear on screen when playing video.
    * Maybe revist later?
    
    This reverts commit df0820a5196be96d6acc5785e69d14218ed4f68f.
    This reverts commit 3693dfdac641a44b175ae092e1a6f56e31e4c319.

--- common.mk
+++ common.mk
@@ -218,8 +218,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
 
 # Graphics
-TARGET_USES_VULKAN = true
-
 PRODUCT_PACKAGES += \
     libgui_vendor
 
